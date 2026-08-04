25b1695223faa760e33fe931ded6d2bc24af87aa
Motapandle Santosh <santoshmotapandle@gmail.com>
2026-08-05 01:02:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
+function swapTheme(){
+	const app = document.getElementById("app");
+	const button = document.getElementById("swap");
 
+	app.classList.toggle("day");
+	app.classList.toogle("night");
+
+	button.classList.toggle("button_day");
+	button.classList.toggle("button_night")
+}
